01ac4fe28d7d23f77684ce3138214a36466fd406
Breakermaker <breakermaker279@gmail.com>
2025-11-02 18:47:44 +0100

    NoMoreStealer

---
+++ NoMoreStealer/internal/Comm/comm.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <fltKernel.h>
+
+#define NMS_SECTION_NAME L"\\BaseNamedObjects\\NoMoreStealerNotify"
+
+namespace NoMoreStealer {
+
+#pragma pack(push, 1)
+    struct NoMoreStealerNotifyData {
+        ULONG pid;
+        USHORT pathLen;      // bytes
+        CHAR procName[64];   // null-terminated
+        ULONG ready;         // 0 = empty, 1 = ready
+    };
+#pragma pack(pop)
+
+    namespace Comm {
+        NTSTATUS Init();
+        VOID Cleanup();
+        VOID NotifyBlock(_In_ PUNICODE_STRING path, _In_opt_ const CHAR* procNameAnsi, _In_ ULONG pid);
+    }
+}
